2967b780f49a35eb300c5809c912ba175d1a5d96
msciborski <michal.sciborski@student.put.poznan.pl>
2019-03-21 20:35:42 +0100

    cors, loggers

--- project/lib/app.ts
+++ project/lib/app.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as cors from "cors";
+import * as morgan from "morgan";
 
 class App {
   public app: express.Application;
@@ -7,11 +9,31 @@ class App {
   constructor() {
     this.app = express();
     this.config();
+    this.configMorgan();
   }
 
   private config() : void {
+    // parsing body of request
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
+
+    //CORS
+    this.app.use(cors())
+  }
+
+  private configMorgan(): void {
+    // Logg error
+    this.app.use(morgan('dev', {
+      skip: (req: Request, res: Response) => res.status < 400,
+      stream: process.stderr,
+    }));
+
+    // Logg success
+    this.app.use(morgan('dev', {
+      skip: (req: Request, res: Response) => res.status >= 400,
+      stream: process.stdout,
+    }));
+
   }
 }
 
