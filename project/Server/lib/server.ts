ef0ea319260de780ee0527e60cc8418dbab43e78
msciborski <michal.sciborski@student.put.poznan.pl>
2019-04-04 20:15:59 +0200

    Move server to server folder

--- project/Server/lib/server.ts
+++ project/Server/lib/server.ts
@@ -1,4 +1,5 @@
 import app from "./app";
+import io from "socket.io";
 import { createServer } from "http";
 
 const PORT = 3000;
@@ -7,4 +8,8 @@ const server = createServer(app);
 server.listen(PORT, () => {
   console.log(`Express is listening on ${PORT}`);
 });
+
+io.listen(server, { log: true })
+  .on('connection', () => console.log('user connected'));
+
 
--- project/lib/socket.ts
+++
@@ -1,4 +0,0 @@
-import io from "socket.io";
-
-export class Socket {
-}
