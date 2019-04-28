657c7f5aaf200962165f5047064683c5ae85bade
msciborski <msciborskI@apollogic.com>
2019-04-28 10:32:40 +0200

    http to https createServer

--- project/Server/lib/server.ts
+++ project/Server/lib/server.ts
@@ -1,9 +1,16 @@
 import app from "./app";
 import * as socketio from "socket.io";
-import { createServer } from "http";
+import { createServer } from "https";
+import { readFileSync } from "fs";
 
 const PORT = 5000;
-const server = createServer(app);
+const server = createServer({
+  key: readFileSync('/etc/letsencrypt/live/nodeference.com/privkey.pem'),
+  cert: readFileSync('/etc/letsencrypt/live/nodeference.com/cert.pem'),
+  ca: readFileSync('/etc/letsencrypt/live/nodeference.com/chain.pem'),
+  requestCert: false,
+  rejectUnauthorized: false,
+}, app);
 let io = require("socket.io")(server);
 
 server.listen(PORT, () => {
